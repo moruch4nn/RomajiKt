dfe37864e3a43c2169f3f4b5276bfde29ea51d6e
moruch4nn <moruch4nn@gmail.com>
2023-06-28 04:12:23 +0900

    ⭐ignoreCaseパラメータを追加

--- src/main/kotlin/RomajiConversion.kt
+++ src/main/kotlin/RomajiConversion.kt
@@ -1,7 +1,11 @@
 object RomajiConversion {
     private val repeatConsonantRegex = Regex("([bcdfghjklmpqrstvwxyz])\\1+")
-    fun romajiToKana(romaji: String): String {
-        var result = romaji
+
+    /**
+     * @param ignoreCase falseの場合は小文字のみ変換/trueの場合はすべての文字を変換
+     */
+    fun romajiToKana(romaji: String, ignoreCase: Boolean): String {
+        var result = if(ignoreCase) romaji.lowercase() else romaji
         repeatConsonantRegex.findAll(result).forEach { matchResult ->
             val value = matchResult.value.dropLast(1)
             result = result.replaceFirst(value, value.replace(value, "っ".repeat(value.length)))
